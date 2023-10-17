76fe886fd99c7878537a89e8a4c8340c9a9b21e6
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-17 15:52:20 +0300

    Create adder_nbit.v

---
+++ N bit adder /adder_nbit.v
@@ -0,0 +1,11 @@
+module adder_nbit
+  //parameter decleration
+  #(parameter N=3)
+  (input [N-1:0] a,
+   input [N-1:0] b,
+   output reg [N:0] sum
+  );
+  
+  always @(*) sum = a + b;
+  
+endmodule 
