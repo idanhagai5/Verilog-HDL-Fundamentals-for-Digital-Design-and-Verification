d40f8f97578ddb0ba950dd4347892097c2963e68
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-17 12:32:08 +0300

    Create testbench.v

---
+++ 1bit Multiplexer/testbench.v
@@ -0,0 +1,26 @@
+module testbench();
+  
+  reg a;
+  reg b;
+  reg sel;
+  wire y;
+  
+ integer i,j,k;
+  
+  //instantiate DUT
+  mux_1bit mux1(a,b,sel,y);
+  
+  
+  initial $monitor("a=%d, b=%d, sel=%d, y=%d",a,b,sel,y);
+
+  initial begin 
+    for (i=0;i<2;i++)  begin
+      for (j=0;j<2;j++) begin
+        for (k=0;k<2;k++) begin
+          	#1 a=i;b=j;sel=k;
+        end
+      end
+    end
+  end
+endmodule
+        
