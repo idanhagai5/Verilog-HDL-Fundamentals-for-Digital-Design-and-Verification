3a7aee690c524fb21372e9d9fcbd7ad12be5109b
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-19 16:45:30 +0300

    Update shift_reg_pipo.v

--- Part 2-Sequential Logic Design/Project12/shift_reg_pipo.v
+++ Part 2-Sequential Logic Design/Project12/shift_reg_pipo.v
@@ -1,1 +1,19 @@
-hift_reg_pipo
+`timescale 1us/1ns 
+module shift_reg_pipo(
+  input reset_n,
+  input clk,
+  input [3:0] d,
+  output reg [3:0] q
+);
+  
+ // Async negative reset_n is used
+  always @(posedge clk or negedge reset_n) begin 
+    if (!reset_n) 
+      q<= 4'b0;
+    else 
+      q[3:0]<=d[3:0];
+   end
+endmodule 
+      
+        
+  
