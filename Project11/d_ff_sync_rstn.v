ab102479cd5de43670e5dd1825524415c0a1e89e
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-19 13:27:19 +0300

    Create d_ff_sync_rstn.v

---
+++ Project11/d_ff_sync_rstn.v
@@ -0,0 +1,20 @@
+`timescale 1us/1ns 
+module d_ff_sync_rstn(
+  input d,
+  input clk,
+  input reset_n,
+  output reg q,
+  output q_not 
+);
+  
+  
+  always @(posedge clk) begin 
+    if(!reset_n)
+      q<=1'b0;
+    else 
+      q<=d;
+  end 
+  
+  assign q_not=~q;
+endmodule
+  
