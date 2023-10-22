05f7d002fc6bc3416116321b130f1aa0bebf4ae2
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-22 13:42:21 +0300

    Update tb_lfsr_16.v

--- Part 2-Sequential Logic Design/Project15/tb_lfsr_16.v
+++ Part 2-Sequential Logic Design/Project15/tb_lfsr_16.v
@@ -1,31 +1,31 @@
 `timescale 1us/1ns 
-module tb_lfsr_16();
+module tb_lfsr_4();
   
   reg reset_n;
   reg clk = 0;
   reg enable;
-  wire [15:0] lfsr;
+  wire [3:0] lfsr;
   
   //instantiate DUT
-  lfsr_16 LFSR (reset_n, clk, enable, lfsr);
+  lfsr_4 LFSR (reset_n, clk, enable, lfsr);
   
     //create a 1MHz clock signal 
   always begin #0.5 clk=~clk; end
   
   initial begin 
-    $monitor ($time,"enable = %b, lfsr = %16b", enable, lfsr);
+    $monitor ($time,"enable = %b, lfsr = %4b", enable, lfsr);
     #1 ; reset_n = 0; enable = 0; // apply reset 
     #1.3 ; reset_n=1; // release reset 
     repeat(2) @(posedge clk) ;
     enable=1;
     
-    repeat(10)  @(posedge clk) ;
+    repeat(16)  @(posedge clk) ;
     enable=0;
   end
   
        initial begin 
-      $dumpfile("lfsr_16.vcd");
-      $dumpvars(0, tb_lfsr_16);
+         $dumpfile("lfsr_4.vcd");
+         $dumpvars(0, tb_lfsr_4);
     #20 $finish; // stop simulator
   end
 endmodule
