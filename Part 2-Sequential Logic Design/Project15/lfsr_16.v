b37fb18fd94a03312b108425ee2665319deb6e8b
Idan Hagai <140309135+idanhagai5@users.noreply.github.com>
2023-10-22 13:41:01 +0300

    Update lfsr_16.v

--- Part 2-Sequential Logic Design/Project15/lfsr_16.v
+++ Part 2-Sequential Logic Design/Project15/lfsr_16.v
@@ -1,21 +1,21 @@
 `timescale 1us/1ns 
-module lfsr_16(
+module lfsr_4(
   input reset_n,
   input clk,
   input enable, // if 1, enables shifting 
-  output reg [15:0] lfsr
+  output reg [3:0] lfsr
 );
   
-  localparam RST_SEED = 16'h1001; 
+  localparam RST_SEED = 4'b1110; 
   wire feedback;
   
-  assign feedback = lfsr[15] ^ lfsr[13] ^ lfsr[12] ^ lfsr[10];
+  assign feedback = lfsr[1] ^ lfsr[3] ;
   
   always @(posedge clk or negedge reset_n) begin
     if (!reset_n)
       lfsr <= RST_SEED;
     else if(enable == 1'b1)
-      lfsr <= {lfsr[14:0],feedback};
+      lfsr <= {lfsr[3:0],feedback};
   end 
 endmodule
       
